f77cdf50fafdfc8934b52e667ec01bac132c273d
JamesTan <jamestan1496@gmail.com>
2025-04-03 16:35:18 +0400

    2nd commit

---
+++ products.js
@@ -0,0 +1,103 @@
+let products = [
+
+    {
+    "id": 1,
+    "title": "Arts and Crafts",
+    "location": "Arts and Crafts",
+    "price": 500,
+    "image": "images/Arts.jpg",
+    "Availability": 10,
+          
+    },
+
+    {
+    "id": 2,
+    "title": "Music and Instruments",
+    "location": "Music and Instruments",
+    "price": 500,
+    "image": "images/Music.jpg",
+    "Availability": 10,
+    
+    },
+
+    {
+    "id": 3,
+    "title": "Sports and Fitness",
+    "location": "Sports and Fitness",
+    "price": 500,
+    "image": "images/Sports.jpg",
+    "Availability": 10,
+          
+    },
+
+    {
+    "id": 4,
+    "title": "Swimming Lessons",
+    "location": "Swimming Lessons",
+    "price": 500,
+    "image": "images/Swimming.jpg",
+    "Availability": 10,
+     
+    },
+
+    {
+    "id": 5,
+    "title": "Performing Arts",
+    "location": "Performing Arts",
+    "price": 500,
+    "image": "images/Performing Arts.jpg",
+    "Availability": 10,
+          
+    },
+    {
+    "id": 6,
+    "title": "Martial Arts",
+    "location": "Martial Arts",
+    "price": 500,
+    "image": "images/Martial arts.jpg",
+    "Availability": 10,
+       
+    },
+
+    {
+    "id": 7,
+    "title": "Culinary Arts",
+    "location": "Culinary Arts",
+    "price": 500,
+    "image": "images/Culinary.jpg",
+    "Availability": 10,
+    
+    },
+
+    {
+    "id": 8,
+    "title": "Foreign Language",
+    "location": "Foreign Language",
+    "price": 500,
+    "image": "images/Foreign.jpg",
+    "Availability": 10,
+       
+    },
+
+    {
+    "id": 9,
+    "title": "Math Lessons",
+    "location": "Math Lessons",
+    "price": 500,
+    "image": "images/Math.jpg",
+    "Availability": 10,
+       
+    },
+
+    {
+    "id": 10,
+    "title": "Video Editing",
+    "location": "Video Editing",
+    "price": 500,
+    "image": "images/video.jpg",
+    "Availability": 10,
+         
+    }
+    
+]  
+
